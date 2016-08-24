172eba05bff7678a29d6272e3e658331d0b97b6d
Shraddha Agrawal (Persistent Systems Ltd.) <v-shragr@microsoft.com>
2016-08-24 13:43:30 +0530

    Added HelloWorld and Twitter asp.net core App to deploy on windows container

---
+++ acs/TwitterApp/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TwitterApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var twitter = new Twitter
+            {
+                OAuthConsumerKey = "G3fdEmo3a2rIHbzR70O7KQij0",
+                OAuthConsumerSecret = "buFAGPjj4ingWBZmEMGYKnRsA0Y4jjQ5yZTXxQyn9obTHTnekq"
+            };
+            
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("<html><body>");
+                IEnumerable<string> twittsClinton = twitter.GetTwitts("Hilary","Clinton",5).Result;
+                IEnumerable<string> twittsTrump = twitter.GetTwitts("Donald", "trump", 5).Result;
+                await context.Response.WriteAsync("<h1 style='color:blue;'>Recent 5 tweets mentioned Hillary clinton</h1>");
+                foreach (var t in twittsClinton)
+                {
+                    await context.Response.WriteAsync(t + "<br>");
+                }
+                await context.Response.WriteAsync("<h1 style='color:blue;'>Recent 5 tweets mentioned Donald trump</h1>");
+                foreach (var t in twittsTrump)
+                {
+                    await context.Response.WriteAsync(t + "<br>");
+                }
+                await context.Response.WriteAsync("</body></html>");
+            });
+        }
+    }
+}
---
+++ acs/TwitterApp/Twitter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace TwitterApp
+{
+    public class Twitter
+    {
+        public string OAuthConsumerSecret { get; set; }        
+        public string OAuthConsumerKey { get; set; }
+
+        public async Task<IEnumerable<string>> GetTwitts(string firstName, string lastName, int count, string accessToken = null)
+        {
+            if (accessToken == null)
+            {
+                accessToken = await GetAccessToken();   
+            }
+
+            var requestUserTimeline = new HttpRequestMessage(HttpMethod.Get, string.Format("https://api.twitter.com/1.1/search/tweets.json?q=%22{0}%20{1}%22&count={2}&result_type=recent", firstName, lastName, count));
+            requestUserTimeline.Headers.Add("Authorization", "Bearer " + accessToken);
+            var httpClient = new HttpClient();
+            HttpResponseMessage responseUserTimeLine = await httpClient.SendAsync(requestUserTimeline);
+            dynamic json = JsonConvert.DeserializeObject(await responseUserTimeLine.Content.ReadAsStringAsync());
+            var statuses = json.statuses;
+            var enumerableTwitts = (statuses as IEnumerable<dynamic>);
+
+            if (enumerableTwitts == null)
+            {
+                return null;
+            }
+            return enumerableTwitts.Select(t => (string)(t["text"].ToString()));                        
+        }
+
+        public async Task<string> GetAccessToken()
+        {           
+            var httpClient = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://api.twitter.com/oauth2/token ");
+            var customerInfo = Convert.ToBase64String(new UTF8Encoding().GetBytes(OAuthConsumerKey + ":" + OAuthConsumerSecret));
+            request.Headers.Add("Authorization", "Basic " + customerInfo);
+            request.Content = new StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+            string json = await response.Content.ReadAsStringAsync();
+            dynamic item = JsonConvert.DeserializeObject(json);
+            return  item["access_token"];            
+        }
+    }
+}
